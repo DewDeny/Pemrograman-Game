8720d4f6f667a8e766c8353ff8ea843f03eeab8d
DewDeny <71222685+DewDeny@users.noreply.github.com>
2020-09-22 10:12:49 +0700

    Add files via upload
    Minggu ke 2 Pemrogame

---
+++ StartMe.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMe : MonoBehaviour {
+	string username = "Deny";
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("My name is "+username+", and I'm not denying the fact that I'm a game dev.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
