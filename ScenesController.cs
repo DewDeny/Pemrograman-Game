cbf0af1f0e045f8c9ff4b34eb671f077b4bd68d9
DewDeny <71222685+DewDeny@users.noreply.github.com>
2020-10-02 15:49:30 +0700

    Add files via upload

---
+++ ScenesController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesController : MonoBehaviour
+{
+	public string EnterScene;
+	public string EscScene;
+	public bool isEscForQuit = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyUp (KeyCode.Return))
+		{
+			Debug.Log ("Name Scene : " + EnterScene);
+			SceneManager.LoadScene (EnterScene);
+		}
+
+		if (Input.GetKeyUp (KeyCode.Escape))
+		{
+			if (isEscForQuit)
+			{
+				Application.Quit ();
+			}
+			else
+			{
+				Debug.Log ("Name Scene : " + EscScene);
+				SceneManager.LoadScene (EscScene);
+			}
+		}
+	}
+}
